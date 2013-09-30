a967f01fbd200c19fa2a60a1a6284dfb4fd9974e
Vvaldir <Vvaldir316@gmail.com>
2013-09-30 17:35:45 -0500

    Create Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,26 @@
+#include "Header.h"
+
+using namespace ATM;
+
+//------------------------------------------------------------------------------//
+
+Customer::Customer(string FN, string LN, int Pin, float Bal, int Acct, vector<string> Trns)
+	: Pin(0), Balance(0), Account(0)
+{}	
+
+//------------------------------------------------------------------------------//
+
+Customer::Customer()						// Default Constructor
+	: Pin(0), Balance(0), Account(0)
+{}
+
+//------------------------------------------------------------------------------//
+
+void TrnsLog(int Account, vector<string> Trns){	
+	for (int i(0); i<(Trns.size()); ++i)
+	{
+		std::cout << Trns[i];
+		if (i%4 == 0 && i != 0) { cout << "\n";}
+		if (i%4 != 0) {cout << "\t";}
+	}
+};
