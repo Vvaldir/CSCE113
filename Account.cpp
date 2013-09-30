272e0b16e87db50c43c40b5921894bf20f64a1fc
Cody Belcher <cody.t.belcher@gmail.com>
2013-09-30 17:38:31 -0500

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -7,4 +7,5 @@ using namespace ATM;
 Account::Account(){/* Initialize the Customer Data from the .txt file */}
 
 //------------------------------------------------------------------------------//
-//test spencer haha
+
+//random comment
