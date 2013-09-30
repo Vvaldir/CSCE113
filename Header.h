a46b128aabae9280cae04afc094014d98d4fd3d4
Vvaldir <Vvaldir316@gmail.com>
2013-09-30 18:26:44 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -46,11 +46,11 @@ struct Statistics{
 	
 	Statistics();
 	
-	void AveBal() {cout << ( Bank::TotalBal(Accts)/Bank::CustomerCt() ) << endl;}	// Prints the average balance in the bank
+	void PrntAveBal() {cout << ( Bank::TotalBal(Accts)/Bank::CustomerCt() ) << endl;}	// Prints the average balance in the bank
 	void PrntCCt() {cout << Bank::CustomerCt() << endl;}					// Prints the number of Customers
 	void PrntTB() {cout << Bank::TotalBal(Bank::Accts) << endl; }					// Prints the total balance in the bank
 	
-}
+};
 
 //------------------------------------------------------------------------------//
 
