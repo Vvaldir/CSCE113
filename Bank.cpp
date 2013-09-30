b2117c0a6cc519b8b6a89b1ee6a2f0fe05cc0ab5
Vvaldir <Vvaldir316@gmail.com>
2013-09-30 17:35:29 -0500

    Create Bank.cpp

---
+++ Bank.cpp
@@ -0,0 +1,9 @@
+#include "Header.h"
+
+using namespace ATM;
+
+//------------------------------------------------------------------------------//
+
+Bank::Bank(){/* Initialize the Vector of Accounts to the data in the .txt file */}
+
+//------------------------------------------------------------------------------//
