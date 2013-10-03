f1245bd50283289380affcfc8e860df5487d258f
Cody Belcher <cody.t.belcher@gmail.com>
2013-10-03 09:42:32 -0500

    Update MyBank.cpp
    Added menu items

--- MyBank.cpp
+++ MyBank.cpp
@@ -1,8 +1,51 @@
-#include "Header.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+void greet()
+{
+    int menu;
+    
+    system("clear");
+    
+    cout<<"Welcome to Pirate Bay Bank, We strive to meet your every need"<<endl<<endl;
+    cout<<"What would ye like to do?"<<endl<<endl;
+    cout<<"\t1) Ope account"<<endl<<endl;
+    cout<<"\t2) Log in"<<endl<<endl;
+    cout<<"\t3) View Bank Stats"<<endl<<endl;
+    
+    cin>>menu;
+    
+    switch (menu)
+    {
+        case 1: 
+            //open();
+            break;
+            
+        case 2:
+            //login();
+            break;
+            
+        case 3:
+            //bank_stats();
+            break;
+            
+        default:
+        {
+            cout<<"Please enter the number of the choice you want followed by the enterkey"<<endl;
+            cin;
+            cin.clear();
+            greet();
+        }
+    }
+}
 
 int main()
 {
-
+    greet();
 
 	
 	return 0;
