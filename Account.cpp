5e579f13026a32541556fcfdae6ae1a3f2c37aba
Vvaldir <Vvaldir316@gmail.com>
2013-10-10 21:00:27 -0500

    Create Account.cpp with some of its baseline stuff

---
+++ Account.cpp
@@ -0,0 +1,26 @@
+#include "Account.h"
+
+using namespace std;
+
+//------------------------------------------------------------------------------//
+
+namespace ATM{
+
+/*  
+Account(Customer Cstmr)
+	:Customer(Cstmr), AcctNumber(to_string(Cstmr::))
+	{  
+      
+	}
+*/  
+Account::Account() 
+{};
+  
+void Transaction();
+void Transaction_Log();
+  	
+};
+
+//------------------------------------------------------------------------------//
+
+}
