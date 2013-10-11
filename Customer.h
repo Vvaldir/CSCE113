961e9eda8caed9fa423b495bc05ac554e9fcde90
Vvaldir <Vvaldir316@gmail.com>
2013-10-10 20:57:45 -0500

    Main Update 1
    Major Overhaul and declaration of many functions!

--- Customer.h
+++ Customer.h
@@ -1,47 +1,77 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
+#ifndef CUSTOMER_GUARD	// if "CUSTOMER_GUARD" isn't defined, then...
+#define CUSTOMER_GUARD	// Define "CUSTOMER_GUARD" as the following code:
+
+#include "Account.h"
 
 using namespace std;
 
-//------------------------------------------------------------------------------//
-
 namespace ATM{
-/*
-struct Bank {
-	Bank();				// Default Constructor
 
-	// Account Management
-	void OpenAcct();
-	void CloseAcct();
+class Customer {            // Also gets data from .txt file
+	// Data Members
+	vector<string> Trns;
+	string First_Name;
+	string Last_Name;
+	int PinNum;
+	float Balance;
+	string Account;	// Really is a member of ATM::Account, just copied over here for easy File I/O applications.
 
-	// Raw Data
-	int CustomerCt() const {return Accts.size();}			// Counts the total number of Customers
-	float TotalBal(vector<Customer> AcctVec);													
+public:
+	// Constructors
+	Customer(string FN, string LN, int Pin, float Bal, string Acct, vector<string> Trns);	// Full Constructor (For Reading from file)
+	Customer(string FN, string LN, int Pin, float Bal, string Acct);	// Standard Constructor
+	Customer();			// Default Constructor
 
-	// Printers
-	/*
-	void AveBal() {cout << ( TotalBal(Accts)/CustomerCt() ) << endl;}	// Prints the average balance in the bank
-	void PrntCCt() {cout << CustomerCt() << endl;}				// Prints the number of Customers
-	void PrntTB() {cout << TotalBal(Accts) << endl; }			// Prints the total balance in the bank
-	*/
-/*private:
-	vector<Customer> Accts;		// The bank is built off of a vector of Accounts
+	// Non-Modifiers
+	vector<string> Trns() const {return Trns;}
+	string Ret_First_Name() const {return First_Name;}
+	string Ret_Last_Name() const {return Last_Name;}
+	int Ret_PinNumber() const {return PinNum;}
+	float Ret_Balance() const {return Balance;}
+	string Ret_Account()const {return Account;}
+
+	// Friends
+	friend struct Account;
+	
+  	// Member Modifiers
+	float Bal_Change(float F) {Balance = Balance+F;}
+
+	// Print Functions
+	void TrnsLog(string Account, vector<string> Trns);				// Prints Transaction Log. Should read info from file into a vector and then print out said vector.
+	void PrintBalance() const {cout << Balance << endl;};
+
+	//Operators	- Only Callable if you do Customer::operatorXX
+	static bool operator>>(istream& is, Customer& C);
+ 	static bool operator<<(ostream& os, Customer& C);
+	static bool operator==(const Customer& C1, const Customer& C2);
+
+	// Called by Account Functions
+	void Withdrawal(float amount);
+	void Deposit(float amount);
 };
-*/
-//------------------------------------------------------------------------------//
-/*		// Obsolete
-struct Account {
-	Account();			// Default Constructor
 
-	void Transaction();		// Responsible for Withdrawal and Deposit Calls in Customer
-	void Transaction_Log();		// Prints out 10 previous Transactions (Calls TrnsLog in Customer)
+//-------[Misc Functions]-----------------------------------------------------//	
+		// Check Format
+bool AcctNumChk(istream& is, string SSN);
+bool PinChk(istream& is, int PinNbmr);
+		// Create Random Values
+string PinGenerator();
+string AcctNumGenerator();
+		// Check for Duplicates
+bool DupAcctChk(string NewAcct);	// Returns false if there is another acct with the same number as NewAcct
 
-private:
-	class Customer;			// An account is built off of a Customer
-};
-*/
-//------------------------------------------------------------------------------//
+//-------[File I/O]-----------------------------------------------------//	    
+void FileWrite(string FileName,vector<Customer> CustVec);
+vector<ATM::Customer> FileRead(string FileName);
+
+//----------------------------------------------------------------------------//
+
+//-------[Operators]------------------------------------------------------------//
+// NOTE: These Functions are defined for file I/O purposes, and should not be used for anything other than file I/O or you may get odd results!		
+bool operator>>(istream& is, Customer& C);
+bool operator<<(ostream& os, Customer& C);
+//bool operator==(const Customer& C1, const Customer& C2);                        
+  
 /*
 struct Statistics{
 	
@@ -53,30 +83,6 @@ struct Statistics{
 	
 };
 */
-//------------------------------------------------------------------------------//
-
-class Customer {
-	Customer(string FN, string LN, int Pin, float Bal, int Acct, vector<string> Trns);		// Constructor
-	Customer();				// Default Constructor
-
-	// Data Members
-	vector<string> Trns;
-	string First_Name;
-	string Last_Name;
-	int Pin;
-	float Balance;
-	int Account;
-
-public:
-	// For use by Account Functions
-	float Withdrawal(float Balance, int Account);
-	float Deposit(float Balance, int Account);
-
-	// Print Functions
-	void TrnsLog(int Account, vector<string> Trns);				// Prints Transaction Log
-	void PrintBalance() const {cout << Balance << endl;};
-};
-
-//------------------------------------------------------------------------------//
-
 }
+
+#endif
